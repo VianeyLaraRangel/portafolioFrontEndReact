80058c0e09801725f64cadbb9e6209d81262b883
VianeyLaraRangel <vianey.lara.rangel@gmail.com>
2019-10-09 02:16:00 -0500

    feat(Components) creando componentes de las partes que componen la landing page, no las considero vistas ya que no es uns SPA es una LP

---
+++ portafolio/src/Components/AboutMe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class About  extends Component {
+    render() {
+        return(
+            <div><h1>Acerca de mí</h1></div>
+        );
+    }
+}
+
+export default About;
