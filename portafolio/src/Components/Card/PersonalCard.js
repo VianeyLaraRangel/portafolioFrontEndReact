9a5e14c63a724b49ed3fbeea551af0d6c2badcf4
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-19 21:50:25 -0500

    styled(bsocial media buttons) Se modifican etiquetas de react-mdl que soportan font awesome icon, se estiliza color y margin para etiquetas i & a

--- portafolio/src/Components/Card/PersonalCard.js
+++ portafolio/src/Components/Card/PersonalCard.js
@@ -1,6 +1,6 @@
 // This components is a child for a view/component LandingPage.
 import React from 'react';
-import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import { Card, CardTitle, CardText, CardActions, FABButton, Icon, IconButton } from 'react-mdl';
 
 class PersonalCard extends React.Component {
     render() {
@@ -18,23 +18,24 @@ class PersonalCard extends React.Component {
                     mi capacidad de auto aprendizaje, adaptabilidad y resolución de problemas
                     para buscar posicionarme activamente en la creación de la industria 4.0.
                 </CardText>
-                <CardActions border className="social-buttons" id="anima">
-                    <Button raised colored>
-                        <a href="../../Documents/vianeyLaraCV.pdf" download="VianeyLaraRangelCV">
-                            <i class="fas fa-file-download"></i>
-                        </a>
-                    </Button>
-                    <Button raised colored>
-                        <a href="https://github.com/VianeyLaraRangel" target="_blank" class="brandico-github">
-                            <i class="fab fa-github"></i>
-                        </a>
-                    </Button>
-                    <Button raised colored>
-                        <a href="https://www.linkedin.com/in/vianeylararangel/" target="_blank" class="brandico-github">
-                            <i class="fab fa-linkedin"></i>
-                        </a>
-                    </Button>
-
+                <CardActions border>
+                    <div className="social-buttons" id="anima">
+                        <FABButton>
+                            <a href="../../src/Documents/vianeyLaraCV.pdf" download="VianeyLaraRangelCV">
+                                <i className="fas fa-file-download"></i>
+                            </a>
+                        </FABButton>
+                        <FABButton>
+                            <a href="https://github.com/VianeyLaraRangel" target="_blank">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </FABButton>
+                        <FABButton>
+                            <a href="https://www.linkedin.com/in/vianeylararangel/" target="_blank">
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </FABButton>
+                    </div>
                 </CardActions>
             </Card>
         );
