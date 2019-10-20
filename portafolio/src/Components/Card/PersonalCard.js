76ffedb4218701ade3669763422322b5524e04e5
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-19 20:55:40 -0500

    chore(social buttons) Se direcciona correctamente a paginas desde botones

--- portafolio/src/Components/Card/PersonalCard.js
+++ portafolio/src/Components/Card/PersonalCard.js
@@ -1,6 +1,6 @@
 // This components is a child for a view/component LandingPage.
 import React from 'react';
-import { Card, CardTitle, CardText, CardActions, Button, Icon } from 'react-mdl';
+import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
 
 class PersonalCard extends React.Component {
     render() {
@@ -9,18 +9,7 @@ class PersonalCard extends React.Component {
                 <CardTitle expand
                     className="card-title">
                     <div className="presentation-box">
-                        <ul class="social-buttons my-3 my-md-3 my-lg-4 mx-auto  " id="anima">
-                            <li>
-                                <a href="docs/cv.pdf" download="BrendaSanreyCV" class="brandico-cv mr-2"><i class="fas fa-file-download align-middle mt-1"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/brendasanrey" target="_blank" class="brandico-github mx-2"><i class="fab fa-github align-middle mt-1"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://www.linkedin.com/in/brendasanrey" target="_blank" class="brandico-link ml-2"> <i
-                                    class="fab fa-linkedin-in align-middle mt-1"></i></a>
-                            </li>
-                        </ul>
+                        <i class="fas fa-map-marker-alt">CDMX</i>
                     </div>
                 </CardTitle>
                 <CardText>
@@ -29,9 +18,22 @@ class PersonalCard extends React.Component {
                     mi capacidad de auto aprendizaje, adaptabilidad y resolución de problemas
                     para buscar posicionarme activamente en la creación de la industria 4.0.
                 </CardText>
-                <CardActions border>
-                    <Button raised colored>Descarga mi CV</Button>
-                    <Button raised colored>Buscame en linkedin</Button>
+                <CardActions border className="social-buttons" id="anima">
+                    <Button raised colored>
+                        <a href="../../Documents/vianeyLaraCV.pdf" download="VianeyLaraRangelCV">
+                            <i class="fas fa-file-download"></i>
+                        </a>
+                    </Button>
+                    <Button raised colored>
+                        <a href="https://github.com/VianeyLaraRangel" target="_blank" class="brandico-github">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </Button>
+                    <Button raised colored>
+                        <a href="https://www.linkedin.com/in/vianeylararangel/" target="_blank" class="brandico-github">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                    </Button>
 
                 </CardActions>
             </Card>
