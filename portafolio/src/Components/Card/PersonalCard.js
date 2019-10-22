34e9c4634a952dddeee5b6eeacf1318b2d2229ab
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-22 18:24:40 -0500

    Commit para sincro

--- portafolio/src/Components/Card/PersonalCard.js
+++ portafolio/src/Components/Card/PersonalCard.js
@@ -8,60 +8,62 @@ import cvViLara from '../../Documents/vianeyLaraCV.pdf';
 class PersonalCard extends React.Component {
     render() {
         return (
-            <div className="card-container">
-                <Card className="card-box">
-                    <CardTitle expand
-                        className="card-title">
-                        <div className="presentation-icon">
-                            <i className="fas fa-map-marker-alt">
-                                <p className="p-fa-map-marker-alt">CDMX</p>
-                            </i>
-                        </div>
-                    </CardTitle>
-                    <CardText className="presentation-box">
-                        <div className="presentation-text">
-                            <p>Ante el panorama de la transformación digital
-                                 y tras años de experiencia en compras, importación y logística
-                                 internacional, decidí continuar desarrollando mi capacidad de
-                                 auto aprendizaje, adaptabilidad y resolución de problemas para
-                                 buscar posicionarme activamente en la creación de la industria 4.0
+            <Card className="card-box">
+                <CardTitle expand
+                    className="card-title">
+                    <section>
+                        <i className="fas fa-map-marker-alt">
+                            <p className="presentation-icon">CDMX</p>
+                        </i>
+                        <i className="fas fa-clock">
+                            <p className="presentation-icon">29 años</p>
+                        </i>
+                        <i className="fas fa-mail-bulk">
+                            <p className="presentation-icon">vianey.lara.rangel@gmail.com</p>
+                        </i>
+                    </section>
+                </CardTitle>
+                <CardText className="presentation-box">
+                    <div className="presentation-text">
+                        <p> 
+                        Analista incansable y autodidacta, ante el panorama de la transformación digital,
+                            decidí comenzar una carrera en tecnología. 
+                        Encontré en la programación web, la oportunidad de aplicar la creatividad,
+                            persistencia e ideación constante con la que cuento.
+                         <strong> Te invito a conocer más de lo que he hecho </strong>
+                          y así encontrar juntxs la forma de 
+                         <strong> colaborar profesionalmente.</strong> 
                         </p>
-                        </div>
-                        <div className="box-logo">
-                            <img
-                                className="logo-brand"
-                                src="https://i.ibb.co/fCqCmGm/logo-Vi-La-Ra2-2-ok.png"
-                                alt="LogoViLaRa" />
-                        </div>
-                    </CardText>
-                    <CardActions border>
-                        <div className="social-buttons" id="anima">
-                            <FABButton>
-                                <a href={cvViLara} download="VianeyLaraRangelCV">
-                                    <i className="fas fa-file-download"></i>
-                                </a>
-                            </FABButton>
-                            <FABButton>
-                                <a href="https://github.com/VianeyLaraRangel" target="_blank" rel="noopener noreferrer">
-                                    <i className="fab fa-github"></i>
-                                </a>
-                            </FABButton>
-                            <FABButton>
-                                <a href="https://www.linkedin.com/in/vianeylararangel/" target="_blank" rel="noopener noreferrer">
-                                    <i className="fab fa-linkedin"></i>
-                                </a>
-                            </FABButton>
-                        </div>
-                    </CardActions>
-                </Card>
-            </div>
+                    </div>
+                    <div className="box-logo">
+                        <img
+                            className="logo-brand"
+                            src="https://i.ibb.co/fCqCmGm/logo-Vi-La-Ra2-2-ok.png"
+                            alt="LogoViLaRa" />
+                    </div>
+                </CardText>
+                <CardActions border>
+                    <div className="social-buttons" id="anima">
+                        <FABButton>
+                            <a href={cvViLara} download="VianeyLaraRangelCV">
+                                <i className="fas fa-file-download"></i>
+                            </a>
+                        </FABButton>
+                        <FABButton>
+                            <a href="https://github.com/VianeyLaraRangel" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </FABButton>
+                        <FABButton>
+                            <a href="https://www.linkedin.com/in/vianeylararangel/" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </FABButton>
+                    </div>
+                </CardActions>
+            </Card>
         );
     }
 }
 
 export default withRouter(PersonalCard);
-
-// handleClick(ev) {
-//     ev.preventDefault();
-//     this.props.history.push('/');
-// }
--- portafolio/src/Components/LandingPage.js
+++ portafolio/src/Components/LandingPage.js
@@ -1,16 +1,14 @@
 //This view shows a quickly view of me
 import React, { Component } from 'react';
-import { Grid } from 'react-mdl';
+// import { Grid } from 'react-mdl';
 
 import PersonalCard from "./Card/PersonalCard";
 
 class Landing extends Component {
     render() {
         return (
-            <div >
-                <Grid className="landing-grid">
-                        <PersonalCard />
-                </Grid>
+            <div className="container">
+                <PersonalCard />
             </div>
         );
     }
