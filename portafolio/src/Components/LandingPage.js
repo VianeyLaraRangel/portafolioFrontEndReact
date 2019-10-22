fe1504f515f746ede9d221efa484b170923a9822
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-22 18:41:53 -0500

    style(personal card) se ajusto el posicionamiento mobile y desktop

--- portafolio/src/Components/LandingPage.js
+++ portafolio/src/Components/LandingPage.js
@@ -1,14 +1,16 @@
 //This view shows a quickly view of me
 import React, { Component } from 'react';
-// import { Grid } from 'react-mdl';
+import { Grid } from 'react-mdl';
 
 import PersonalCard from "./Card/PersonalCard";
 
 class Landing extends Component {
     render() {
         return (
-            <div className="container">
-                <PersonalCard />
+            <div >
+                <Grid className="landing-grid">
+                        <PersonalCard />
+                </Grid>
             </div>
         );
     }
