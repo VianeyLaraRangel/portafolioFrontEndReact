192043b4950f93d38db679a2d8e0199a5045fa54
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-25 18:16:10 -0500

    feat(Contact-whatsapp button)se prueba crear un  boton que referecie al link oficial de whatsapp para que te envien un mensaje

--- portafolio/src/Components/Contact.js
+++ portafolio/src/Components/Contact.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { FABButton } from 'react-mdl';
+// import { Link } from 'react-router-dom';
 
-class Contact  extends Component {
+class Contact extends Component {
     render() {
-        return(
-            <div><h1>Contacto</h1></div>
+        return (
+            <FABButton>
+                <a href="https://api.whatsapp.com/send?phone=5521013057&text=&source=&data=" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-whatsapp"></i>
+                </a>
+            </FABButton>
         );
     }
 }
