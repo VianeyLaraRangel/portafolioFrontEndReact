11b0828e3e3e79422effa5a8d98bb8bb6d2e8891
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-25 01:31:43 -0500

    style(homeButton) fondo desktop rojo

--- portafolio/src/Components/AboutMe.js
+++ portafolio/src/Components/AboutMe.js
@@ -6,20 +6,27 @@ class About extends Component {
             <div className="container-aboutme">
                 <section className="aboutme-text">
                     <p>
+                        Ante el panorama de la transformación digital
+                        <strong> decidí hacer carrera en la tecnología </strong>
+                        para posicionarme activamente en la creación de la industria 4.0.
+                    </p>
+                    <p>
                         En los <strong>últimos 6 meses</strong> me convertí en
                         <strong> Front-end </strong> developer de la <em>8va generación de Laboratoria </em>.
                     </p>
+                    <ul>
+                       Los retos han sido:
+                       <li>Desarrollar habilidades técnicas (tech skills) gracias a la práctica orientada a proyectos.</li> 
+                       <li>Comprender y aplicar <em>"mentalidad de crecimiento"</em></li>
+                        <li>Aplicar la retroalimentación constante sobre habilidades psicoemocionales(soft skills)
+                         para <em>ser programadora</em> y aprovechar mi previa experiencia profesional.
+                        </li>
+                    </ul>
                     <p>
                         Estudio <strong>los Sábados la licenciatura en Derecho</strong> en Ciudad Universitaria (UNAM SUA).
                     </p>
-                    <p>
-                        Ante el panorama de la transformación digital 
-                        <strong> decidí hacer carrera en la tecnología </strong>
-                        y así posicionarme activamente en la creación de la industria 4.0.
-                    </p>
-                    <p> 
-                    desarrollar nuevas habilidades "soft" y mejorar profundamente las relacionadas a mi experiencia profesional previa.
-                    </p>
+                    <p>Me considero empática, proactiva, analítica y persistente, siempre estoy dispuesta a ayudar a otrxs 
+                        ya que <u>transmitir y recibir conocimiento</u> me encanta, el intercambio de ideas y observar desde todos los ángulos es una de mis pasiones. </p>
                 </section>
             </div>
         );
