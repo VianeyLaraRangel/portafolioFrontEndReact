eeee37e983760a158066d6938eb183cf36459db9
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-29 17:02:23 -0600

    chore(projectCards) Se maqueta cada una de las cards que se rquieren para cada uno de los proyectos

--- portafolio/src/Components/Card/ProyectCard.js
+++ portafolio/src/Components/Card/ProyectCard.js
@@ -1,25 +1,103 @@
 //This component es a child for a view/component Projects
 import React from 'react';
-import { Card, CardTitle, CardText, CardActions, Button, CardMenu , IconButton } from 'react-mdl';
+import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
 import { withRouter } from 'react-router';
+
+import html from '../../Assets/html5Logo.png';
 
 class ProjectCard extends React.Component {
     render() {
         return (
-            <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
-                <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>Welcome</CardTitle>
-                <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Mauris sagittis pellentesque lacus eleifend lacinia...
+            <div>
+                <div>
+                    {/* DataLovers Card  */}
+                    <Card shadow={0} style={{ width: '20rem', margin: '1rem' }}>
+                        <CardTitle style={{ color: '#000000', height: '15rem', background: 'url(https://i.ibb.co/12Shwsd/pokepedia-Screenshot.png) left / cover' }}>
+                        </CardTitle>
+                        <CardText className="project-card">
+                            <h6>POKEPEDIA</h6>
+                            Responsive webApp que permite filtrar por tipo y debilidades el listado de la 1° generacion de pokemones. Dirigido a nostálgicos de la serie.
                 </CardText>
-                <CardActions border>
-                    <Button colored>DEMO</Button>
-                    <Button colored>CODIGO FUENTE</Button>
-                </CardActions>
-                <CardMenu style={{ color: '#fff' }}>
-                    <IconButton name="share" />
-                </CardMenu>
-            </Card>
+                        <CardText className="project-technologies">
+                            <img src={html} alt="tecnologias-logo" className="img-tech"></img>
+                        </CardText>
+                        <CardActions>
+                            <Button colored href="https://vianeylararangel.github.io/MEX-Data-Lovers-008/src/index.html" target="_blank">
+                                DEMO
+                    </Button>
+                            <Button colored href="https://github.com/VianeyLaraRangel/MEX-Data-Lovers-008" target="_blank">
+                                CODIGO FUENTE
+                    </Button>
+                        </CardActions>
+                    </Card>
+                </div>
+                {/* Social Network Card */}
+                <div>
+                    <Card shadow={0} style={{ width: '20rem', margin: '1rem' }}>
+                        <CardTitle style={{ color: '#000000', height: '15rem', background: 'url(https://i.ibb.co/12Shwsd/pokepedia-Screenshot.png) left / cover' }}>
+                        </CardTitle>
+                        <CardText className="project-card">
+                            <h6>POKEPEDIA</h6>
+                            Responsive webApp que permite filtrar por tipo y debilidades el listado de la 1° generacion de pokemones. Dirigido a nostálgicos de la serie.
+                        </CardText>
+                        <CardText className="project-technologies">
+                            <img src={html} alt="tecnologias-logo" className="img-tech"></img>
+                        </CardText>
+                        <CardActions>
+                            <Button colored href="https://vianeylararangel.github.io/MEX-Data-Lovers-008/src/index.html" target="_blank">
+                                DEMO
+                            </Button>
+                            <Button colored href="https://github.com/VianeyLaraRangel/MEX-Data-Lovers-008" target="_blank">
+                                CODIGO FUENTE
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </div>
+                {/* This4That Card */}
+                <div>
+                    <Card shadow={0} style={{ width: '20rem', margin: '1rem' }}>
+                        <CardTitle style={{ color: '#000000', height: '15rem', background: 'url(https://i.ibb.co/12Shwsd/pokepedia-Screenshot.png) left / cover' }}>
+                        </CardTitle>
+                        <CardText className="project-card">
+                            <h6>POKEPEDIA</h6>
+                            Responsive webApp que permite filtrar por tipo y debilidades el listado de la 1° generacion de pokemones. Dirigido a nostálgicos de la serie.
+                        </CardText>
+                        <CardText className="project-technologies">
+                            <img src={html} alt="tecnologias-logo" className="img-tech"></img>
+                        </CardText>
+                        <CardActions>
+                            <Button colored href="https://vianeylararangel.github.io/MEX-Data-Lovers-008/src/index.html" target="_blank">
+                                DEMO
+                            </Button>
+                            <Button colored href="https://github.com/VianeyLaraRangel/MEX-Data-Lovers-008" target="_blank">
+                                CODIGO FUENTE
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </div>
+                {/*BurguerQueen Card */}
+                <div>
+                    <Card shadow={0} style={{ width: '20rem', margin: '1rem' }}>
+                        <CardTitle style={{ color: '#000000', height: '15rem', background: 'url(https://i.ibb.co/12Shwsd/pokepedia-Screenshot.png) left / cover' }}>
+                        </CardTitle>
+                        <CardText className="project-card">
+                            <h6>POKEPEDIA</h6>
+                            Responsive webApp que permite filtrar por tipo y debilidades el listado de la 1° generacion de pokemones. Dirigido a nostálgicos de la serie.
+                        </CardText>
+                        <CardText className="project-technologies">
+                            <img src={html} alt="tecnologias-logo" className="img-tech"></img>
+                        </CardText>
+                        <CardActions>
+                            <Button colored href="https://vianeylararangel.github.io/MEX-Data-Lovers-008/src/index.html" target="_blank">
+                                DEMO
+                            </Button>
+                            <Button colored href="https://github.com/VianeyLaraRangel/MEX-Data-Lovers-008" target="_blank">
+                                CODIGO FUENTE
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </div>
+            </div>
         );
     }
 }
