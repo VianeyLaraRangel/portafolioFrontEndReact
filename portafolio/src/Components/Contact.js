64dfb129147500ed824340a3b984bc4dd6131f20
ViLaRa <vianey.lara.rangel@gmail.com>
2019-11-12 00:30:39 -0600

    feat(social media buttons) Se agregan íconos a redes sociales en contact me view

--- portafolio/src/Components/Contact.js
+++ portafolio/src/Components/Contact.js
@@ -6,14 +6,43 @@ class Contact extends Component {
     render() {
         return (
             <div>
+                <p>¿Buscas una desarrolladora o tienes 
+                    alguna pregunta acerca de mi trabajo? 
+                    Contáctame.
+                </p>
                 <FABButton >
-                    <a href="https://api.whatsapp.com/send?phone=5521013057&text=&source=&data=" target="_blank" rel="noopener noreferrer">
+                    <a href="https://api.whatsapp.com/send?phone=5521013057&text=&source=&data=" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
                         <i className="fab fa-whatsapp"></i>
                     </a>
                 </FABButton>
                 <FABButton >
-                    <a href="mailto:vianey.lara.rangel@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <a href="mailto:vianey.lara.rangel@gmail.com" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
                         <i className="far fa-envelope"></i>
+                    </a>
+                </FABButton>
+                <FABButton >
+                    <a href="https://www.facebook.com/mrs.vinicia" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </FABButton>
+                <FABButton >
+                    <a href="https://medium.com/@vianey.lara.rangel" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                        <i class="fab fa-medium"></i>
+                    </a>
+                </FABButton>
+                <FABButton >
+                    <a href="https://twitter.com/mrsvinicia" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                        <i class="fab fa-twitter"></i>
                     </a>
                 </FABButton>
             </div>
